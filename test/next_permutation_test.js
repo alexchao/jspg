6d852ccdb683aee58f201cdde22cdca5c9ad96a3
Alex Chao <alex.h.chao@gmail.com>
2017-07-29 17:37:31 -0400

    Make getNextPermutation tests more readable

--- test/next_permutation_test.js
+++ test/next_permutation_test.js
@@ -10,21 +10,21 @@ describe('getNextPermutation()', function() {
     assert.equal(getNextPermutation([5, 3, 2]), null);
   });
   it('should return [5, 2] for [2, 5]', function() {
-    assert.deepEqual(getNextPermutation([2, 5]), [5, 2]);
+    assert.deepEqual([5, 2], getNextPermutation([2, 5]));
   });
   it('should return [1, 3, 5, 2] for [1, 3, 2, 5]', function() {
-    assert.deepEqual(getNextPermutation([1, 3, 2, 5]), [1, 3, 5, 2]);
+    assert.deepEqual([1, 3, 5, 2], getNextPermutation([1, 3, 2, 5]));
   });
   it('should return [1,6,2,4,7] for [1,4,7,6,2]', function() {
-    assert.deepEqual(getNextPermutation([1,4,7,6,2]), [1,6,2,4,7]);
+    assert.deepEqual([1,6,2,4,7], getNextPermutation([1,4,7,6,2]));
   });
   it('should return [6,9,2,1,4,7] for [6,9,1,7,4,2]', function() {
-    assert.deepEqual(getNextPermutation([6,9,1,7,4,2]), [6,9,2,1,4,7]);
+    assert.deepEqual([6,9,2,1,4,7], getNextPermutation([6,9,1,7,4,2]));
   });
   it('should return [6,7,2,4,4] for [6,4,7,4,2]', function() {
-    assert.deepEqual(getNextPermutation([6,4,7,4,2]), [6,7,2,4,4]);
+    assert.deepEqual([6,7,2,4,4], getNextPermutation([6,4,7,4,2]));
   });
   it('should return [4,2,3,5,5] for [3,5,5,4,2]', function() {
-    assert.deepEqual(getNextPermutation([3,5,5,4,2]), [4,2,3,5,5]);
+    assert.deepEqual([4,2,3,5,5], getNextPermutation([3,5,5,4,2]));
   });
 });
